327db31b9a39287390c89c8fadc156d088fc6f46
RickDev <98334211+r1ckd3v@users.noreply.github.com>
2025-08-20 11:30:41 -0400

    feat: Add CORS header
    CORS headers added

--- app.js
+++ app.js
@@ -9,6 +9,17 @@ const PORT = 8080;
 // initialize body-parser for application/json
 app.use(bodyParser.json());
 
+// CORS Headers
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET, POST, PUT, PATCH, DELETE'
+  );
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
+
 // all the routes that starts by /main will be attended by this router
 app.use('/main', mainRoutes);
 
